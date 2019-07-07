3e531e764af6b2d4a55a70b35813cd173a1ee476
Fuyang Liu <fuyangl@spotify.com>
2019-07-07 22:44:16 +0200

    Let each thead merge map directly after reach read

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![feature(duration_float)]
-use std::time::Instant;
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 use std::path::PathBuf;
 use std::thread;
+use std::time::Instant;
 
-use crossbeam::crossbeam_channel::{bounded, Sender, Receiver};
+use crossbeam::crossbeam_channel::{unbounded, bounded, Receiver, Sender};
 use rayon::prelude::*;
 
 type Map = HashMap<String, u32>;
@@ -13,28 +13,31 @@ type Map = HashMap<String, u32>;
 fn main() {
     let now = Instant::now();
 
-    let (chan_path_s, chan_path_r) = bounded(100);
-    let (chan_map_s, chan_map_r) = bounded(100);
-    
+    let (chan_path_s, chan_path_r) = unbounded(); // channel to store all the paths to read
+    let (chan_map_s, chan_map_r) = bounded(16); // channel to store maps of word count
+
     thread::spawn(|| gen_paths(chan_path_s));
 
-    for _ in 0..8 {
+    for _ in 0..16 {
         let r = chan_path_r.clone();
         let s = chan_map_s.clone();
         thread::spawn(|| read_and_count(r, s));
     }
-    drop(chan_map_s);
+    drop(chan_map_s); // close map channel from current thead, so to allow the collection on receiver to continue 
 
     // read all from map channel into a list
-    let v : Vec<_>= chan_map_r.iter().collect();
-    println!("number of files/maps: {}", v.len());
+    let maps: Vec<Map> = chan_map_r.iter().collect();
+    println!("number of maps: {}", maps.len());
 
     // run parallel to reduce into a single map
-    let r = v.into_par_iter().reduce_with(merge_maps).unwrap();
-    println!("number of unique words: {}", r.len());
+    let result = maps.into_par_iter().reduce_with(merge_maps).unwrap();
+    println!("number of unique words: {}", result.len());
 
     println!("count finished {}", now.elapsed().as_secs_f32());
 }
+// output:
+// number of unique words: 101703
+// count finished 0.75196064
 
 fn gen_paths(sender: Sender<PathBuf>) {
     for entry in fs::read_dir("./aclImdb/train/pos").unwrap() {
@@ -60,14 +63,14 @@ fn gen_paths(sender: Sender<PathBuf>) {
     drop(sender); // end of sending, close the channel
 }
 
-fn read_and_count(
-    chan_path_r: Receiver<PathBuf>, 
-    chan_map_s: Sender<Map>) {
+fn read_and_count(chan_path_r: Receiver<PathBuf>, chan_map_s: Sender<Map>) {
+    let mut map = Map::new();
     while let Ok(path) = chan_path_r.recv() {
         let contents = fs::read_to_string(path).expect("file reading error");
-        let map = word_count(contents.as_ref());
-        chan_map_s.send(map).unwrap();
+        let map2 = word_count(contents.as_ref());
+        map = merge_maps(map, map2);
     }
+    chan_map_s.send(map).unwrap();
     drop(chan_map_s); // end of sending, close the channel
 }
 
@@ -79,16 +82,17 @@ fn merge_maps(mut a: Map, b: Map) -> Map {
 }
 
 pub fn word_count(sentence: &str) -> HashMap<String, u32> {
-    sentence.split(|c: char| !c.is_alphanumeric())
+    sentence
+        .split(|c: char| !c.is_alphanumeric())
         .filter(|w| !w.is_empty())
         .map(|w| w.to_lowercase())
         .fold(HashMap::new(), count)
 }
 
-fn count(mut hash_map: HashMap<String, u32>, word: String) -> HashMap<String, u32> {
+fn count(mut map: HashMap<String, u32>, word: String) -> HashMap<String, u32> {
     {
-        let c = hash_map.entry(word).or_insert(0);
-        *c += 1;
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
-    hash_map
+    map
 }
